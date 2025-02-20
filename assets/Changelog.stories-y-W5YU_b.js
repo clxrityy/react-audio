1211c2c22445dbcba488821be379d47d6e2dce39
clxrityy <clxrityxyz@gmail.com>
2025-02-20 16:10:20 -0500

    Updates

---
+++ assets/Changelog.stories-y-W5YU_b.js
@@ -0,0 +1,158 @@
+import{u as s,j as a}from"./index-ODlto6PU.js";import"./dialog-DwnSQ0AV.js";function n(e){const l={code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",strong:"strong",ul:"ul",...s(),...e.components};return a.jsxs(a.Fragment,{children:[a.jsx(l.h1,{className:"ladle-markdown",children:"@clxrity/react-audio"}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"1.7.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["f5abd36: added ",a.jsx(l.strong,{className:"ladle-markdown",children:"AudioUploader"})," functional with no UI display yet"]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["d89e093: add ",a.jsx(l.strong,{className:"ladle-markdown",children:"resize()"})," function to ",a.jsx(l.strong,{className:"ladle-markdown",children:"Waveform"}),"'s canvas if no size prop is present"]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["23839c0: ",a.jsx(l.strong,{className:"ladle-markdown",children:"AudioInputVisualizer"})," works fully functional"]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["ba70a93: began ",a.jsx(l.strong,{className:"ladle-markdown",children:"AudioInput"})," component where you can input audio mic/guitar"]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["bb9f2b5: added prop to ",a.jsx(l.strong,{className:"ladle-markdown",children:"Waveform"})," & ",a.jsx(l.strong,{className:"ladle-markdown",children:"AudioInputVisualizer"}),": fftsize"]}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"be63b43: re-added imports for libraryplayer & librarytrackitem"}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"1.6.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"9ba3169: update props setup, all now have specific inherited props"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"0422a54: update storybook"}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"ad13785: fix imports and props passing on"}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"1.5.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"dbc2980: add docs"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"7d930b0: add nextjs app to docs"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"e56a5f9: added a script that generates an api route for creating a waveform image"}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"1.4.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"832989e: added jest, testing ffmpeg features to create waveforms"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"f7f4d6f: restructure files (exports in src/lib/)"}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"c650b27: make all exports default"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"610d33d: fixed jest interfering with rollup build"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"3a33cf0: ability to change color in waveforms actually works now"}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"1.3.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"48caa05: change styled-components to devDependency"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"7560f63: update packages"}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["0dff7df: add ",a.jsx(l.strong,{className:"ladle-markdown",children:"showTrackInfo"})," prop to ",a.jsx(l.strong,{className:"ladle-markdown",children:"Waveform"})]}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"aa2aaa1: remove tailwindcss dependency"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"3bd6fe5: fix progress bar on waveform"}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.14.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"cd32bde: more styling updates to library"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"2061b0b: update readme"}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["596d485: add other components to ",a.jsx(l.strong,{className:"ladle-markdown",children:"Waveform"})]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["e95dde9: updated styles for ",a.jsx(l.strong,{className:"ladle-markdown",children:"AudioPlayer"})," (removed unused thumbnail)"]}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["cb432a6: ",a.jsx(l.strong,{className:"ladle-markdown",children:"Waveform"})," is functional, no errors, need to add other component parts"]}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"8ef7746: waveform now works"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"ce890a9: fix dom errors"}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.13.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["1659bd6: begin ",a.jsx(l.strong,{className:"ladle-markdown",children:"Waveform"}),` component, existing error: "Uncaught DOMException: Failed to execute 'createMediaElementSource' on 'AudioContext': HTMLMediaElement already connected previously to a different MediaElementSourceNode."`]}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"fdf559b: style updates to library player"}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.11.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"39ed441: add loading states to every component"}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:['7a5aca2: fix error "rounded is not defined" prop with ',a.jsx(l.strong,{className:"ladle-markdown",children:"Button"})]}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.10.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["c1c9610: removed ",a.jsx(l.strong,{className:"ladle-markdown",children:"AudioPlayer"})," for now"]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["9bb0c8d: added (begun) ",a.jsx(l.strong,{className:"ladle-markdown",children:a.jsx(l.code,{className:"ladle-markdown",children:"<AudioPlayer />"})})]}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:['8cd2ac2: fix "Audio is not defined" error in ',a.jsx(l.strong,{className:"ladle-markdown",children:a.jsx(l.code,{className:"ladle-markdown",children:"<JustPlayer />"})})]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["4af6757: fix ",a.jsx(l.strong,{className:"ladle-markdown",children:"JustPlayer"}),": no more useEffect issue"]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:['213a90c: fixed "variant" unknown prop in ',a.jsx(l.strong,{className:"ladle-markdown",children:a.jsx(l.code,{className:"ladle-markdown",children:"<Button />"})}),", will find better ways to update styles and theming"]}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.9.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["4f368d5: ",a.jsx(l.strong,{className:"ladle-markdown",children:"AudioLibrary"})," is fully functional and styled nicely for now, updated readme & package.json"]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["1f5711c: change library component to ",a.jsx(l.strong,{className:"ladle-markdown",children:"AudioLibrary"})," with states and styles inside"]}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.8.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["469f488: add / begin ",a.jsx(l.code,{className:"ladle-markdown",children:"<LibraryPlayer />"})," component"]}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"aec2e66: add styled-components"}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["60d76fd: ",a.jsx(l.code,{className:"ladle-markdown",children:"<LibraryPlayer />"})," now works (somewhat)"]}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.7.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"62c85bf: update packages"}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["443185b: ",a.jsx(l.code,{className:"ladle-markdown",children:"<JustPlayer />"})," now works (plays audio)"]}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.6.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["6e7d46f: updated visuals to ",a.jsx(l.code,{className:"ladle-markdown",children:"<AudioPlayer />"}),", began ",a.jsx(l.code,{className:"ladle-markdown",children:"<JustPlayer />"})]}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.5.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"3dc1f94: add background color param"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"df5f580: add license and npmignore"}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"fc106f5: it works now"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"df5f580: make changeset a dev dependency"}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.4.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["d184bff: made component export & import into applications properly, ",a.jsx(l.code,{className:"ladle-markdown",children:"<AudioPlayer />"})," component is now (semi) functional and visible"]}),`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["f5c0d15: structuring ",a.jsx(l.code,{className:"ladle-markdown",children:"AudioPlayer"})," component. began exporting compatible types for the (optional) options (styling so far)"]}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.3.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"af6c419: test tailwindcss"}),`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"7cacb07: begin functionality of audio component"}),`
+`]}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Patch Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsxs(l.li,{className:"ladle-markdown",children:["7cacb07: change name of ",a.jsx(l.code,{className:"ladle-markdown",children:"Player"})," to ",a.jsx(l.code,{className:"ladle-markdown",children:"AudioPlayer"})]}),`
+`]}),`
+`,a.jsx(l.h2,{className:"ladle-markdown",children:"0.2.0"}),`
+`,a.jsx(l.h3,{className:"ladle-markdown",children:"Minor Changes"}),`
+`,a.jsxs(l.ul,{className:"ladle-markdown",children:[`
+`,a.jsx(l.li,{className:"ladle-markdown",children:"begin testing, add changeset, begin development process"}),`
+`]})]})}function d(e={}){const{wrapper:l}={...s(),...e.components};return l?a.jsx(l,{...e,children:a.jsx(n,{...e})}):n(e)}d.storyName="Readme";typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{d as MDXContent};
