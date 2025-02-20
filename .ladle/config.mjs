7e3de90a863423852c4fdc3af4f776226f14f138
clxrityy <clxrityxyz@gmail.com>
2025-02-20 17:37:03 -0500

    I DONT KNOW

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -10,14 +10,13 @@ export default {
   },
   viteConfig: process.cwd() + "/vite.ladle.config.ts",
   previewPort: 3000,
-  host: "clxrityy.github.io/react-audio",
   staticDirs: ['public'],
   appendToHead: `
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+        <link rel="manifest" href="./manifest.json" />
         <meta name="mobile-web-app-capable" content="yes" />
         <title>@clxrity/react-audio</title>
     `,
