25c48990a3133368d44d05c1fcbf1c82180169aa
clxrityy <clxrityxyz@gmail.com>
2025-08-23 01:03:23 -0400

    fix

--- apps/docs/app/docs/[[...mdxPath]]/page.tsx
+++ apps/docs/app/docs/[[...mdxPath]]/page.tsx
@@ -30,10 +30,10 @@ export default async function Page(props: Props): Promise<JSX.Element> {
   const params = await props.params;
   const result = await importPage(params.mdxPath);
 
-  const { default: MDXContent, toc, metadata } = result;
+  const { default: MDXContent, toc, metadata, sourceCode } = result;
 
   return (
-    <Wrapper toc={toc} metadata={metadata}>
+    <Wrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
       <MDXContent {...props} params={params} />
     </Wrapper>
   );
